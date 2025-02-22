f79eb252c85bce32bdf74244ac1fc12c02748a76
Joan Amengual <joan@consilientlabs.com>
2025-02-22 14:13:58 +0100

    Mas ficheros

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -18,7 +18,7 @@ public class Application {
     public void init()
     {
         Logger log = LoggerFactory.getLogger(Application.class);
-        log.info("App de Java iniciada");
+        log.info("Java app started");
     }
 
     public String getStatus() {
